0f2050e83552084db06bf584d4afb4ad5647b0e7
Gordeev Vladimir <gordeev.vladimir.v@gmail.com>
2013-03-03 23:34:34 +0300

    add ParaType fonts load from Google Web Fonts

--- code.js
+++ code.js
@@ -1,3 +1,23 @@
+WebFontConfig = {
+	google: {
+		families: [
+			'PT+Sans:400,700:latin,cyrillic',
+			'PT+Sans+Narrow:400,700:latin,cyrillic',
+			'PT+Mono::latin,cyrillic'
+		]
+	}
+};
+
+(function() {
+	var wf = document.createElement('script');
+	wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+		'://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+	wf.type = 'text/javascript';
+	wf.async = 'true';
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(wf, s);
+})();
+
 var setupLangTitle = function () {
 	$('.sh_sourceCode').each(function (i, e) {
 		console.log('hoo')
